4f59f4ec2a51277125d84b6f82029fc4e1c58c3b
jonaszeihe <jonas.zeihe@gmail.com>
2025-08-04 18:13:27 +0200

    Change basename lol

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from './components/Header'
 import ScrollToTopButton from './components/ScrollToTopButton'
 
 const App = () => (
-  <Router basename="/casestudy">
+  <Router basename="/flavorfusion/casestudy">
     {' '}
     <GlobalStyles />
     <Header />
